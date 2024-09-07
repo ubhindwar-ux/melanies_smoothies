fd440f6354fc90b60e25638866bf58a6081fa98d
Uday Bhindwar <92682305+ubhindwar-ux@users.noreply.github.com>
2024-09-07 19:32:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
 name_on_order = st.text_input('Name of Smoothie:')
 st.write('The name of your Smoothie will be: ', name_on_order)
 
-cnx = st.connection('showflake')
+cnx = st.connection('snowflake')
 session = cnx.session()
 
 #session = get_active_session()
