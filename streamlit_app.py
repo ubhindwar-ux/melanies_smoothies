0339faf7fccd5052cbc4fed2cf4bdf005ea2b7eb
Uday Bhindwar <92682305+ubhindwar-ux@users.noreply.github.com>
2024-09-07 19:48:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie !:cup_with_straw:")
 st.write(
